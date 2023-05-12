c534053b1bc42d2360fd88cb282af8ba824285bd
shanto <santawkha@gmail.com>
2023-05-13 02:57:28 +0600

    toast button added

--- src/ChefDetails.jsx
+++ src/ChefDetails.jsx
@@ -2,12 +2,20 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from './provider/AuthProvider';
 import { useParams } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Card } from 'react-bootstrap';
 
 
 const ChefDetails = (chef) => {
     const [details, setDetails] = useState([]);
+    const [show, setShow] = useState(false);
     const { id } = useParams();
+
+    const handleFavButton = (event) => {
+        event.currentTarget.disabled = true;
+        toast('Added Favourite Button')
+    }
     useEffect(() => {
         fetch(`http://localhost:5000/chefData/${id}`)
             .then(res => res.json())
@@ -21,7 +29,7 @@ const ChefDetails = (chef) => {
         <div>
 
             <Card className="bg-dark text-white">
-                <Card.Img src="../public/images/card.avif" alt="Card image" />
+                <Card.Img src="/public/images/card.avif" alt="Card image" />
                 <Card.ImgOverlay>
                     <Card.Title className='text-center mt-5 text-4xl'>Chef Details</Card.Title>
                     <Card.Text>
@@ -29,11 +37,12 @@ const ChefDetails = (chef) => {
                         <p className='mt-4'>Chef Name: {details.chef_name}</p>
                         <p>{details.description}</p>
                         <p>Ratings: 5</p>
-                        <button className='btn btn primary'>Add To Favourite</button>
+                        <div><button onClick={handleFavButton} className='btn btn-primary'>Add To Favourite</button> <ToastContainer /></div>
                     </Card.Text>
                     <Card.Text>Last updated 3 mins ago</Card.Text>
                 </Card.ImgOverlay>
             </Card>
+
 
         </div>
     );
