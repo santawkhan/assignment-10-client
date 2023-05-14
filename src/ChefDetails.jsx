f892af70a5387f7338f10776522b2ea58828cf75
shanto <santawkha@gmail.com>
2023-05-14 12:23:01 +0600

    readmi file added

--- src/ChefDetails.jsx
+++ src/ChefDetails.jsx
@@ -5,7 +5,6 @@ import { useParams } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Card, Col, Row } from 'react-bootstrap';
-import cardImg from './assets/images/card.avif'
 
 
 const ChefDetails = (chef) => {
@@ -18,7 +17,7 @@ const ChefDetails = (chef) => {
         toast('Added Favourite Button')
     }
     useEffect(() => {
-        fetch(`https://chef-website-server-santawkhan.vercel.app/chefData/${id}`)
+        fetch(`http://localhost:5000/chefData/${id}`)
             .then(res => res.json())
             .then(data => setDetails(data.item))
     }, [])
@@ -29,69 +28,67 @@ const ChefDetails = (chef) => {
     return (
         <div>
 
-            <Card className="bg-dark text-white ">
-                <Card.Img src={cardImg} className='h-100 alt="Card image" />
+            <Card className="bg-dark text-white">
+                <Card.Img src="/public/images/card.avif" alt="Card image" />
                 <Card.ImgOverlay>
                     <Card.Title className='text-center mt-5 text-4xl'>Chef Details</Card.Title>
-            <Card.Text>
-                <img src={details.chef_picture} className='text-center w-25 h-25 mx-auto rounded' alt="" />
-                <p className='mt-4'>Chef Name: {details.chef_name}</p>
-                <p>{details.description}</p>
-                <p>Ratings: 5</p>
-
-            </Card.Text>
-        </Card.ImgOverlay>
-            </Card >
-
-    <div className='mt-4 '><Row xs={1} md={2} lg={2} className="g-4 mb-3">
-
-        <Col >
-            <Card className='shadow p-3 mb-5  rounded zoom bg-danger-subtle border border-primary'>
-
-                <Card.Body>
-                    <Card.Title>{details.recipe_name_one}</Card.Title>
                     <Card.Text>
-                        <p> <h4>Ingredients:</h4>{details.ingredients_one}</p>
-                        <p className='fw-bold'>Cooking Method </p>
-                        <p>{details.cooking_method_one}</p>
+                        <img src={details.chef_picture} className='text-center w-25 h-25 mx-auto rounded' alt="" />
+                        <p className='mt-4'>Chef Name: {details.chef_name}</p>
+                        <p>{details.description}</p>
+                        <p>Ratings: 5</p>
                         <div><button onClick={handleFavButton} className='btn btn-primary'>Add To Favourite</button> <ToastContainer /></div>
                     </Card.Text>
-                </Card.Body>
+                    <Card.Text>Last updated 3 mins ago</Card.Text>
+                </Card.ImgOverlay>
             </Card>
-        </Col>
-        <Col >
-            <Card className='shadow p-3 mb-5 bg-body-tertiary rounded zoom border border-primary '>
 
-                <Card.Body>
-                    <Card.Title> <h3>{details.recipe_name_two}</h3> </Card.Title>
-                    <Card.Text>
-                        <p> <h4>Ingredients:</h4>{details.ingredients_three}</p>
-                        <p className='fw-bold'>Cooking Method </p>
-                        <p>{details.cooking_method_two}</p>
-                        <div><button onClick={handleFavButton} className='btn btn-primary'>Add To Favourite</button> <ToastContainer /></div>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </Col>
-        <Col >
-            <Card className='shadow p-3 mb-5 bg-body-tertiary rounded zoom border border-primary'>
+            <div className='mt-4 '><Row xs={1} md={2} lg={2} className="g-4 mb-3">
 
-                <Card.Body>
-                    <Card.Title>{details.recipe_name_three}</Card.Title>
-                    <Card.Text>
-                        <p> <h4>Ingredients:</h4>{details.ingredients_three}</p>
-                        <p className='fw-bold'>Cooking Method </p>
-                        <p>{details.cooking_method_two}</p>
-                        <div><button onClick={handleFavButton} className='btn btn-primary'>Add To FFavourite</button> <ToastContainer /></div>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        </Col>
+                <Col >
+                    <Card className='shadow p-3 mb-5  rounded zoom bg-danger-subtle border border-primary'>
+
+                        <Card.Body>
+                            <Card.Title>{details.recipe_name_one}</Card.Title>
+                            <Card.Text>
+                                <p> <h4>Ingredients:</h4>{details.ingredients_one}</p>
+                                <p className='fw-bold'>Cooking Method </p>
+                                <p>{details.cooking_method_one}</p>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col >
+                    <Card className='shadow p-3 mb-5 bg-body-tertiary rounded zoom border border-primary '>
+
+                        <Card.Body>
+                            <Card.Title> <h3>{details.recipe_name_two}</h3> </Card.Title>
+                            <Card.Text>
+                                <p> <h4>Ingredients:</h4>{details.ingredients_three}</p>
+                                <p className='fw-bold'>Cooking Method </p>
+                                <p>{details.cooking_method_two}</p>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col >
+                    <Card className='shadow p-3 mb-5 bg-body-tertiary rounded zoom border border-primary'>
+
+                        <Card.Body>
+                            <Card.Title>{details.recipe_name_three}</Card.Title>
+                            <Card.Text>
+                                <p> <h4>Ingredients:</h4>{details.ingredients_three}</p>
+                                <p className='fw-bold'>Cooking Method </p>
+                                <p>{details.cooking_method_two}</p>
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
 
 
 
-    </Row></div>
-        </div >
+            </Row></div>
+        </div>
     );
 };
 
